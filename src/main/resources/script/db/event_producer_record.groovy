270f0d6fdc1d965577a55a527ec363459ea26178
HuangFuqiang <fuqianghuang01@gmail.com>
2018-07-09 17:17:58 +0800

    [ADD]创建版本发送kafka消息

---
+++ src/main/resources/script/db/event_producer_record.groovy
@@ -0,0 +1,15 @@
+package db
+
+databaseChangeLog(logicalFilePath: 'event_producer_record.groovy') {
+    changeSet(id: '2018-07-09-add-table-event-producer-record', author: 'fuqianghuang01@gmail.com') {
+        createTable(tableName: "event_producer_record") {
+            column(name: 'uuid', type: 'VARCHAR(50)', autoIncrement: false, remarks: 'uuid') {
+                constraints(primaryKey: true)
+            }
+            column(name: 'type', type: 'VARCHAR(50)', remarks: '业务类型') {
+                constraints(nullable: false)
+            }
+            column(name: 'create_time', type: 'DATETIME', remarks: '创建时间')
+        }
+    }
+}
