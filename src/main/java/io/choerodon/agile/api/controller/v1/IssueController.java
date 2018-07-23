e277c53f2065d1cfb970c51d9e3f5eefb4bd7cb7
HuangFuqiang <fuqianghuang01@gmail.com>
2018-07-23 15:48:03 +0800

    [FIX]修改api注释

--- src/main/java/io/choerodon/agile/api/controller/v1/IssueController.java
+++ src/main/java/io/choerodon/agile/api/controller/v1/IssueController.java
@@ -130,7 +130,7 @@ public class IssueController {
     }
 
     @Permission(level = ResourceLevel.PROJECT, roles = {InitRoleCode.PROJECT_MEMBER, InitRoleCode.PROJECT_OWNER})
-    @ApiOperation("分页搜索查询issue列表(不包含子任务)")
+    @ApiOperation("分页搜索查询issue列表(包含子任务)")
     @CustomPageRequest
     @GetMapping(value = "/summary")
     public ResponseEntity<Page<IssueNumDTO>> queryIssueByOption(@ApiIgnore
