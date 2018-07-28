8453638fb766438ada954131950b24989355ac8c
dinghuang <dinghuang123@gmail.com>
2018-07-28 09:24:13 +0800

    [IMP] 饼图返回经办人信息修改

--- src/main/java/io/choerodon/agile/app/service/impl/ReportServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/ReportServiceImpl.java
@@ -889,20 +889,19 @@ public class ReportServiceImpl implements ReportService {
         List<PieChartDTO> pieChartDTOList = reportAssembler.pieChartDoToDto(pieChartDOS);
         if (pieChartDTOList != null && !pieChartDTOList.isEmpty()) {
             List<Long> userIds = pieChartDTOList.stream().filter(pieChartDTO ->
-                    pieChartDTO.getName() != null && !"0".equals(pieChartDTO.getName())).map(pieChartDTO ->
-                    Long.parseLong(pieChartDTO.getName())).collect(Collectors.toList());
+                    pieChartDTO.getTypeName() != null && !"0".equals(pieChartDTO.getTypeName())).map(pieChartDTO ->
+                    Long.parseLong(pieChartDTO.getTypeName())).collect(Collectors.toList());
             Map<Long, UserMessageDO> usersMap = userRepository.queryUsersMap(userIds, true);
             pieChartDTOList.parallelStream().forEach(pieChartDTO -> {
                 JSONObject jsonObject = new JSONObject();
-                if (pieChartDTO.getName() != null && usersMap.get(Long.parseLong(pieChartDTO.getName())) != null) {
-                    String assigneeName = usersMap.get(Long.parseLong(pieChartDTO.getName())).getName();
-                    String assigneeImageUrl = usersMap.get(Long.parseLong(pieChartDTO.getName())).getImageUrl();
-                    String email = usersMap.get(Long.parseLong(pieChartDTO.getName())).getEmail();
-                    jsonObject.put("assigneeName", assigneeName);
+                if (pieChartDTO.getTypeName() != null && usersMap.get(Long.parseLong(pieChartDTO.getTypeName())) != null) {
+                    String assigneeName = usersMap.get(Long.parseLong(pieChartDTO.getTypeName())).getName();
+                    String assigneeImageUrl = usersMap.get(Long.parseLong(pieChartDTO.getTypeName())).getImageUrl();
+                    String email = usersMap.get(Long.parseLong(pieChartDTO.getTypeName())).getEmail();
+                    pieChartDTO.setName(assigneeName);
                     jsonObject.put("assigneeImageUrl", assigneeImageUrl);
                     jsonObject.put("email", email);
                 } else {
-                    jsonObject.put("assigneeName", null);
                     jsonObject.put("assigneeImageUrl", null);
                     jsonObject.put("email", null);
                 }
