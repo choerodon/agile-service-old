08ce756201cc5b9c62153dde7aee0ec27e91b246
HuangFuqiang <fuqianghuang01@gmail.com>
2018-07-30 21:52:05 +0800

    [FIX]去除无用的引入包

--- src/main/java/io/choerodon/agile/app/service/impl/ReportServiceImpl.java
+++ src/main/java/io/choerodon/agile/app/service/impl/ReportServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Function;
