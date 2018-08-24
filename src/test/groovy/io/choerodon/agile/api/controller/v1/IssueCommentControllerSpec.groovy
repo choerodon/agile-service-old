6a0211851f95c52d576a1689acad3cb0bc56123f
devil-scp <1293496808@qq.com>
2018-08-24 17:58:46 +0800

    [ADD]添加IssueComentControler和IssueComponentController测试用例

---
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/IssueCommentControllerSpec.groovy
@@ -0,0 +1,175 @@
+package io.choerodon.agile.api.controller.v1
+
+import com.alibaba.fastjson.JSONObject
+import io.choerodon.agile.AgileTestConfiguration
+import io.choerodon.agile.api.dto.IssueCommentCreateDTO
+import io.choerodon.agile.api.dto.IssueCommentDTO
+import io.choerodon.agile.domain.agile.repository.UserRepository
+import io.choerodon.agile.infra.dataobject.UserDO
+import io.choerodon.agile.infra.dataobject.UserMessageDO
+import io.choerodon.agile.infra.mapper.IssueCommentMapper
+import io.choerodon.agile.infra.mapper.IssueMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+/**
+ * Creator: scp
+ * Date:  9:44 2018/8/24
+ * Description:
+ */
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Import(AgileTestConfiguration)
+@ActiveProfiles("test")
+@Stepwise
+class IssueCommentControllerSpec extends Specification {
+    @Autowired
+    TestRestTemplate restTemplate
+
+    @Autowired
+    IssueMapper issueMapper
+
+    @Autowired
+    IssueCommentMapper issueCommentMapper
+
+    @Autowired
+    @Qualifier("mockUserRepository")
+    private UserRepository userRepository
+
+    @Shared
+    def projectId = 1
+
+    def setup() {
+        given: '设置feign调用mockito'
+        // *_表示任何长度的参数（这里表示只要执行了queryUsersMap这个方法，就让它返回一个空的Map
+        Map<Long, UserMessageDO> userMessageDOMap = new HashMap<>()
+        UserMessageDO userMessageDO = new UserMessageDO("管理员", "http://XXX.png", "dinghuang123@gmail.com")
+        userMessageDOMap.put(1, userMessageDO)
+        userRepository.queryUsersMap(*_) >> userMessageDOMap
+        UserDO userDO = new UserDO()
+        userDO.setRealName("管理员")
+        userRepository.queryUserNameByOption(*_) >> userDO
+
+    }
+
+    /*
+    def 'createIssueComment'() {
+        given: '准备IssueCommentCreateDTO'
+        List<IssueCommentCreateDTO> list = new ArrayList<>()
+        IssueCommentCreateDTO createDTO = new IssueCommentCreateDTO()
+        createDTO.issueId = 1L
+        createDTO.commentText = '这是一条测试评论'
+        list.add(createDTO)
+        IssueCommentCreateDTO createDTO1 = new IssueCommentCreateDTO()
+        createDTO1.issueId = 10L
+        createDTO1.commentText = '这是一条测试评论1'
+        list.add(createDTO1)
+
+        when: '发送创建issue评论请求'
+        def entity = restTemplate.postForEntity('/v1/projects/{project_id}/issue_comment', test, IssueCommentDTO, projectId)
+
+        then: '请求结果'
+        entity.statusCode.toString() == resultCode
+        entity.body.commentText == resultComment
+
+        where: '期望值'
+        test       | resultCode | resultComment
+        createDTO  | 200        | '这是一条测试评论'
+        createDTO1 | 500        | null
+    }*/
+
+    def 'createIssueComment'() {
+        given: '准备IssueCommentCreateDTO'
+        IssueCommentCreateDTO issueCommentCreateDTO = new IssueCommentCreateDTO()
+        issueCommentCreateDTO.issueId = 1L
+        issueCommentCreateDTO.commentText = '这是一条测试评论'
+
+
+        when: '发送创建issue评论请求'
+        def entity = restTemplate.postForEntity('/v1/projects/{project_id}/issue_comment', issueCommentCreateDTO, IssueCommentDTO, projectId)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+
+        and: '设置值'
+        IssueCommentDTO issueCommentDTO = entity.body
+
+        expect: '设置期望值'
+        issueCommentDTO.commentText == issueCommentCreateDTO.commentText
+        issueCommentDTO.issueId == issueCommentCreateDTO.issueId
+        issueCommentMapper.selectAll().size() == 1
+    }
+
+    def 'updateIssueComment'() {
+        given: '更新IssueComment'
+        JSONObject issueCommentUpdate = new JSONObject()
+        and: '设置更新issueComment的信息'
+        issueCommentUpdate.put("commentId", 1L)
+        issueCommentUpdate.put("commentText", '这是一条更新评论')
+        issueCommentUpdate.put("objectVersionNumber", 1L)
+
+        when: '发送更新issue评论请求'
+        def entity = restTemplate.postForEntity('/v1/projects/{project_id}/issue_comment/update', issueCommentUpdate, IssueCommentDTO, projectId)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+
+        and: '设置值'
+        IssueCommentDTO issueCommentDTO = entity.body
+
+        expect: '设置期望值'
+        issueCommentDTO.commentText == "这是一条更新评论"
+        issueCommentMapper.selectAll().size() == 1
+    }
+
+    def 'queryIssueCommentList'() {
+        when: '发送查询issue评论请求'
+        def entity = restTemplate.getForEntity('/v1/projects/{project_id}/issue_comment/{issueId}', List, projectId, issueId)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+        List<IssueCommentDTO> result = entity.body
+
+
+        and: '设置值'
+        result.size() == expectSize
+
+        where: '期望值'
+        issueId | expectSize
+        1L      | 1
+        2L      | 0
+        10L     | 0
+    }
+
+    def 'deleteIssueComment'() {
+        when: '发送查询issue评论请求'
+        def entity = restTemplate.exchange("/v1/projects/{project_id}/issue_comment/{issueId}",
+                HttpMethod.DELETE,
+                null,
+                ResponseEntity.class,
+                projectId,
+                commentId
+        )
+
+        then: '返回值'
+        entity.statusCode.is2xxSuccessful()
+
+        and: '设置值'
+        issueCommentMapper.selectByPrimaryKey(commentId) == expectObject
+
+        where: '期望值'
+        commentId | expectObject
+        1L        | null
+    }
+
+}
---
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/IssueComponentControllerSpec.groovy
@@ -0,0 +1,204 @@
+package io.choerodon.agile.api.controller.v1
+
+import io.choerodon.agile.AgileTestConfiguration
+import io.choerodon.agile.api.dto.ComponentForListDTO
+import io.choerodon.agile.api.dto.IssueComponentDTO
+import io.choerodon.agile.api.dto.IssueDTO
+import io.choerodon.agile.domain.agile.repository.UserRepository
+import io.choerodon.agile.infra.dataobject.IssueComponentDO
+import io.choerodon.agile.infra.dataobject.UserDO
+import io.choerodon.agile.infra.dataobject.UserMessageDO
+import io.choerodon.agile.infra.mapper.IssueComponentMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.context.annotation.Import
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+/**
+ * Creator: scp
+ * Date:  13:50 2018/8/24
+ * Description:
+ */
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Import(AgileTestConfiguration)
+@ActiveProfiles("test")
+@Stepwise
+class IssueComponentControllerSpec extends Specification {
+    @Autowired
+    TestRestTemplate restTemplate
+
+    @Autowired
+    IssueComponentMapper issueComponentMapper
+
+    @Autowired
+    @Qualifier("mockUserRepository")
+    private UserRepository userRepository
+
+    @Shared
+    def projectId = 1L
+
+    @Shared
+    def componentId = 1L
+
+    def setup() {
+        given: '设置feign调用mockito'
+        // *_表示任何长度的参数（这里表示只要执行了queryUsersMap这个方法，就让它返回一个空的Map
+        Map<Long, UserMessageDO> userMessageDOMap = new HashMap<>()
+        UserMessageDO userMessageDO = new UserMessageDO("管理员", "http://XXX.png", "dinghuang123@gmail.com")
+        userMessageDOMap.put(1, userMessageDO)
+        userRepository.queryUsersMap(*_) >> userMessageDOMap
+        UserDO userDO = new UserDO()
+        userDO.setRealName("管理员")
+        userRepository.queryUserNameByOption(*_) >> userDO
+
+    }
+
+    def 'createComponent'() {
+        given: '准备IssueCommentCreateDTO'
+        IssueComponentDTO issueComponentDTO = new IssueComponentDTO()
+        issueComponentDTO.projectId = 1L
+        issueComponentDTO.name = 'test_component'
+        issueComponentDTO.description = 'this is a description'
+        issueComponentDTO.managerId = 1L
+        issueComponentDTO.defaultAssigneeRole = 'this is a defaultAssigneeRole'
+
+        when: '发送创建component请求'
+        def entity = restTemplate.postForEntity('/v1/projects/{project_id}/component', issueComponentDTO, IssueComponentDTO, projectId)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+
+        and: '设置值'
+        IssueComponentDTO resultIssueComponentDTO = entity.body
+
+        expect: '设置期望值'
+        resultIssueComponentDTO.projectId == issueComponentDTO.projectId
+        resultIssueComponentDTO.name == issueComponentDTO.name
+        resultIssueComponentDTO.description == issueComponentDTO.description
+        resultIssueComponentDTO.managerId == issueComponentDTO.managerId
+        resultIssueComponentDTO.defaultAssigneeRole == issueComponentDTO.defaultAssigneeRole
+    }
+
+    def 'updateComponent'() {
+        given: '修改component'
+        List<IssueComponentDO> list = issueComponentMapper.selectAll()
+        IssueComponentDO issueComponentDO = list.get(1)
+        componentId = issueComponentDO.componentId
+        IssueComponentDTO issueComponentDTO = new IssueComponentDTO()
+        issueComponentDTO.projectId = issueComponentDO.projectId
+        issueComponentDTO.name = issueComponentDO.name
+        issueComponentDTO.managerId = issueComponentDO.managerId
+        issueComponentDTO.objectVersionNumber = issueComponentDO.objectVersionNumber
+        issueComponentDTO.description = "this is a description for update"
+        issueComponentDTO.defaultAssigneeRole = "this is a defaultAssigneeRole for update"
+
+        when: '发送更新component请求'
+        HttpEntity<IssueComponentDTO> requestEntity = new HttpEntity<IssueComponentDTO>(issueComponentDTO, null)
+        def entity = restTemplate.exchange("/v1/projects/{project_id}/component/{id}",
+                HttpMethod.PUT,
+                requestEntity,
+                IssueComponentDTO,
+                projectId,
+                componentId
+        )
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+
+        and: '设置值'
+        IssueComponentDTO resultIssueComponentDTO = entity.body
+
+        expect: '设置期望值'
+        resultIssueComponentDTO.projectId == issueComponentDO.projectId
+        resultIssueComponentDTO.name == issueComponentDO.name
+        resultIssueComponentDTO.description == "this is a description for update"
+        resultIssueComponentDTO.managerId == issueComponentDO.managerId
+        resultIssueComponentDTO.defaultAssigneeRole == "this is a defaultAssigneeRole for update"
+    }
+
+
+    def 'queryComponentById'() {
+        when: '发送查询component请求'
+        def entity = restTemplate.getForEntity('/v1/projects/{project_id}/component/{id}', IssueComponentDTO, projectId, componentId)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+
+
+        and: '设置值'
+        IssueComponentDTO result = entity.body
+
+        expect: "设置期望值"
+        result.componentId == componentId
+        result.projectId == projectId
+    }
+
+
+    def 'listByProjectId'() {
+        when: '根据project id查询component'
+        def entity = restTemplate.getForEntity('/v1/projects/{project_id}/component', List, projectIds, componentId, noIssueTest)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+        List<ComponentForListDTO> result = entity.body
+
+        and: '设置值'
+        result.size() == expectSize
+
+        where: '期望值'
+        projectIds | noIssueTest | expectSize
+        1L         | true        | 2
+        2L         | true        | 0
+        10L        | true        | 0
+    }
+
+    def 'listByOptions'() {
+        when: '根据id查询component下的issues'
+        def entity = restTemplate.getForEntity('/v1/projects/{project_id}/component/{id}/issues', List, projectId, componentIds)
+
+        then: '请求结果'
+        entity.statusCode.is2xxSuccessful()
+        List<IssueDTO> result = entity.body
+
+        and: '设置值'
+        result.size() == expectSize
+
+        where: '期望值'
+        componentIds | expectSize
+        1L           | 0
+        2L           | 0
+        10L          | 0
+    }
+
+
+    def 'deleteComponent'() {
+        when: '删除component'
+        def entity = restTemplate.exchange("/v1/projects/{project_id}/component/{id}",
+                HttpMethod.DELETE,
+                null,
+                ResponseEntity.class,
+                projectId,
+                componentId,
+                relateComponentId
+        )
+
+        then: '返回值'
+        entity.statusCode == expectCode
+
+        where: '期望值'
+        relateComponentId | expectCode
+        1L                | HttpStatus.NO_CONTENT
+    }
+
+}
