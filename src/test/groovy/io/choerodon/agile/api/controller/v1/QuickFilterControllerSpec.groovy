a8bf9f55c45a19c12a1375679fdce4b0436387f2
devil-scp <changpingshi0213@gmail.com>
2018-08-31 17:18:14 +0800

    [ADD] 添加IssueLinkTypeController测试用例

---
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/QuickFilterControllerSpec.groovy
@@ -0,0 +1,11 @@
+package io.choerodon.agile.api.controller.v1
+
+import spock.lang.Specification
+
+/**
+ * Creator: scp
+ * Date:  16:19 2018/8/31
+ * Description:
+ */
+class QuickFilterControllerSpec extends Specification {
+}
