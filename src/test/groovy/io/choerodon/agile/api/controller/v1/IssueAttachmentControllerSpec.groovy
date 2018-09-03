209f2a8bb01008ad1245c6b95c91653449e5e802
devil-scp <changpingshi0213@gmail.com>
2018-09-03 09:34:28 +0800

    [ADD] 添加IssueAttachment文件上传测试用例

--- src/test/groovy/io/choerodon/agile/api/controller/v1/IssueAttachmentControllerSpec.groovy
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/IssueAttachmentControllerSpec.groovy
@@ -1,9 +1,8 @@
 package io.choerodon.agile.api.controller.v1
 
-import com.alibaba.fastjson.JSONObject
 import io.choerodon.agile.AgileTestConfiguration
 import io.choerodon.agile.api.dto.IssueAttachmentDTO
-import io.choerodon.agile.infra.dataobject.IssueAttachmentDO
+import io.choerodon.agile.app.service.IssueAttachmentService
 import io.choerodon.agile.infra.feign.FileFeignClient
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,22 +10,23 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Import
+import org.springframework.core.io.FileSystemResource
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MvcResult
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.multipart.MultipartFile
-
-import static org.mockito.Matchers.anyString
-import static org.mockito.Matchers.any
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
+
+import static org.mockito.Matchers.anyString
+import static org.mockito.Matchers.any
 
 import javax.servlet.http.HttpServletRequest
 
@@ -55,6 +55,10 @@ class IssueAttachmentControllerSpec extends Specification {
     @Qualifier("fileFeignClient")
     private FileFeignClient fileFeignClient
 
+    @Autowired
+    @Qualifier("issueAttachmentService")
+    IssueAttachmentService issueAttachmentService
+
     @Shared
     def projectId = 1
 
@@ -63,15 +67,26 @@ class IssueAttachmentControllerSpec extends Specification {
 
     def 'uploadAttachment'() {
         given: '上传附件'
-        MockMultipartFile mockMultipartFile =
-                new MockMultipartFile("file", "FileUploadTest.txt", "text/plain", "This is a Test".getBytes())
-        Mockito.when(fileFeignClient.uploadFile(anyString(), anyString(),any(MultipartFile.class))).thenReturn(new ResponseEntity<>(
+        Mockito.when(fileFeignClient.uploadFile(anyString(), anyString(), any(MultipartFile.class))).thenReturn(new ResponseEntity<>(
                 "https://minio.choerodon.com.cn/agile-service/file_56a005f56a584047b538d5bf84b17d70_blob.png", HttpStatus.OK))
-        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.fileUpload("/v1/projects/1/issue_attachment?issueId=1")
-                .file(mockMultipartFile)).andReturn()
-        List<IssueAttachmentDTO> issueAttachmentDTOList =  JSONObject.parseArray(result.getResponse().content.toString(),IssueAttachmentDTO)
-        expect: '设置期望值'
-        issueAttachmentDTOList.get(0).projectId == 1
+
+        FileSystemResource resource = new FileSystemResource(new File( "D:\\test1.txt"))
+        MultiValueMap<String, Object> param = new LinkedMultiValueMap<>()
+        param.add("file", resource)
+        param.add("fileName", "test1.txt")
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<MultiValueMap<String, Object>>(param, null)
+
+        when: '发送请求'
+        def entity = restTemplate.exchange("/v1/projects/1/issue_attachment?issueId=1", HttpMethod.POST, httpEntity, List)
+
+        then: '返回值'
+        entity.statusCode.is2xxSuccessful()
+
+        and: '设置值'
+        List<IssueAttachmentDTO> list = entity.body
+
+        expect: '期望值'
+        list.size() == 1
     }
 }
+
