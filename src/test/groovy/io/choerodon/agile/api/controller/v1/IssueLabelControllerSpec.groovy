5a22ecc14cfd93b30a801e328c19663c56cdce74
devil-scp <changpingshi0213@gmail.com>
2018-09-03 10:52:12 +0800

    [ADD]删除IssueLabelControllerSpec

--- src/test/groovy/io/choerodon/agile/api/controller/v1/IssueLabelControllerSpec.groovy
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/IssueLabelControllerSpec.groovy
@@ -2,8 +2,9 @@ package io.choerodon.agile.api.controller.v1
 
 import io.choerodon.agile.AgileTestConfiguration
 import io.choerodon.agile.api.dto.IssueLabelDTO
-import io.choerodon.agile.infra.dataobject.IssueSprintRelDO
-import io.choerodon.agile.infra.mapper.IssueSprintRelMapper
+import io.choerodon.agile.infra.dataobject.LabelIssueRelDO
+import io.choerodon.agile.infra.mapper.IssueMapper
+import io.choerodon.agile.infra.mapper.LabelIssueRelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -29,37 +30,50 @@ class IssueLabelControllerSpec extends Specification {
     TestRestTemplate restTemplate
 
     @Autowired
-    IssueSprintRelMapper issueSprintRelMapper
+    IssueMapper issueMapper
+
+    @Autowired
+    LabelIssueRelMapper labelIssueRelMapper
 
     @Shared
     def realConut = 0
 
-    def 'listIssueLinkByIssueId'() {
-        when: '发送请求'
-        def entity = restTemplate.getForEntity('/v1/projects/{project_id}/issue_labels', List, projectIds)
+//    def 'listIssueLinkByIssueId'() {
+//        when: '发送请求'
+//        realConut = 0
+//        def entity = restTemplate.getForEntity('/v1/projects/{project_id}/issue_labels', List, projectIds)
+//
+//        then: '返回值'
+//        entity.statusCode.is2xxSuccessful()
+//
+//        and: '设置值'
+//        List<IssueLabelDTO> result = entity.body
+//        if (result.size() == 0) {
+////            IssueDO issueDO = new IssueDO()
+////            issueDO.projectId = projectIds
+////            List<IssueDO> listIssueDOResult = issueMapper.select(issueDO)
+////            if (listIssueDOResult.size() != 0) {
+////                for (int i = 0; i < listIssueDOResult.size(); i++) {
+//            LabelIssueRelDO labelIssueRelDO = new LabelIssueRelDO()
+//            LabelIssueRelDO.labelName = '测试标签'
+//            if (labelIssueRelMapper.selectOne(labelIssueRelDO) != null) {
+//                realConut = realConut + 1
+//            }
+////                }
+////            }
+//        } else {
+//            realConut = result.size()
+//        }
+//
+//        and: '设置期望值'
+//        realConut == expectConut
+//
+//        where: '给定参数'
+//        projectIds | expectConut
+//        1L         | 1
+//        2L         | 0
+//        20L        | 0
+//
+//    }
 
-        then: '返回值'
-        entity.statusCode.is2xxSuccessful()
-
-        and: '设置值'
-        List<IssueLabelDTO> result = entity.body
-        if (result.size() == 0) {
-            IssueSprintRelDO issueSprintRelDO = new IssueSprintRelDO()
-            issueSprintRelDO.projectId = projectIds
-            if (issueSprintRelMapper.selectOne(issueSprintRelDO) != null) {
-                realConut = 1
-            } else {
-                realConut = 0
-            }
-        }
-
-        and: '设置期望值'
-        realConut == expectConut
-
-        where: '给定参数'
-        projectIds | expectConut
-        1L         | 1
-        2L         | 0
-
-    }
 }
