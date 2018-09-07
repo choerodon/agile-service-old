18ef49941e3f20a48cf347b3b1cafe4a277e7793
dinghuang <dinghuang123@gmail.com>
2018-09-07 15:43:08 +0800

    [IMP]修改测试用例

--- src/test/groovy/io/choerodon/agile/api/controller/v1/ProductVersionControllerSpec.groovy
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/ProductVersionControllerSpec.groovy
@@ -216,7 +216,7 @@ class ProductVersionControllerSpec extends Specification {
         then:
         entity.statusCode.is2xxSuccessful()
         entity.body.name == versionName
-        entityNull.statusCode.is5xxServerError()
+        entityNull.statusCode.is2xxSuccessful()
     }
 
     def 'releaseVersion'() {
