68a91969ef1acc30aee34bc655217b6cf8cb184a
HuangFuqiang <fuqianghuang01@gmail.com>
2018-09-11 09:34:50 +0800

    [FIX]修改epic日志记录

--- src/main/java/io/choerodon/agile/infra/common/aspect/DataLogAspect.java
+++ src/main/java/io/choerodon/agile/infra/common/aspect/DataLogAspect.java
@@ -1233,7 +1233,9 @@ public class DataLogAspect {
 
     private void dataLogCreateEpicId(Long epicId, IssueDO originIssueDO, ProjectInfoDO projectInfoDO) {
         IssueDO issueEpic = queryIssueByIssueIdAndProjectId(originIssueDO.getProjectId(), epicId);
-        if (issueEpic != null) {
+        if (issueEpic == null) {
+            throw new CommonException(ERROR_EPIC_NOT_FOUND);
+        } else {
             redisUtil.deleteRedisCache(new String[]{EPIC_CHART + originIssueDO.getProjectId() + ":" + issueEpic.getIssueId() + ":" + "*",
                     PIECHART + originIssueDO.getProjectId() + ':' + "epic"
             });
