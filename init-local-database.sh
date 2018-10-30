2a66add752b359e8b2d25a58c51005e739bb5729
HuangFuqiang <huangfuqiang@HuangFuqiangdeMacBook-Pro.local>
2018-10-30 21:25:49 +0800

    [FIX]修改故事地图epic查询接口

--- init-local-database.sh
+++ init-local-database.sh
@@ -5,8 +5,8 @@ then
     curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.5.2.RELEASE/choerodon-tool-liquibase-0.5.2.RELEASE.jar -L  -o target/choerodon-tool-liquibase.jar
 fi
 java -Dspring.datasource.url="jdbc:mysql://localhost/agile_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
- -Dspring.datasource.username=root \
- -Dspring.datasource.password=test123 \
+ -Dspring.datasource.username=choerodon \
+ -Dspring.datasource.password=123456 \
  -Ddata.drop=false -Ddata.init=true \
  -Ddata.dir=src/main/resources \
  -jar target/choerodon-tool-liquibase.jar
