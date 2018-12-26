b8cd73aa9e85fedb5b0e79ff11f7e3d627d3ace7
dinghuang <dinghuang123@gmail.com>
2018-12-26 15:14:03 +0800

    [IMP] 修改测试用例

--- src/test/groovy/io/choerodon/agile/api/controller/v1/IssueControllerSpec.groovy
+++ src/test/groovy/io/choerodon/agile/api/controller/v1/IssueControllerSpec.groovy
@@ -1245,10 +1245,8 @@ class IssueControllerSpec extends Specification {
 
         and:
         List<Long> result = entity.body
-        List<IssueDO> issueDOList = issueMapper.queryIssueByIssueIdsAndSubIssueIds(result)
 
         expect:
-        issueDOList.size() == 12
         result.size() == 12
     }
 
