8a723bd06b8501bce454c0dd2128d9958d633a52
dinghuang <dinghuang123@gmail.com>
2019-01-08 15:35:45 +0800

    [ADD] add 0.13.0 changelog and modify fix cumulative

--- src/main/java/io/choerodon/agile/api/controller/v1/FixDataController.java
+++ src/main/java/io/choerodon/agile/api/controller/v1/FixDataController.java
@@ -1,16 +1,16 @@
 package io.choerodon.agile.api.controller.v1;
 
 import io.choerodon.agile.app.service.IssueStatusService;
+import io.choerodon.agile.app.service.ReportService;
 import io.choerodon.core.iam.InitRoleCode;
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.swagger.annotation.Permission;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by HuangFuqiang@choerodon.io on 2018/11/13.
@@ -22,6 +22,8 @@ public class FixDataController {
 
     @Autowired
     private IssueStatusService issueStatusService;
+    @Autowired
+    private ReportService reportService;
 
     @Permission(level = ResourceLevel.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR, InitRoleCode.SITE_DEVELOPER})
     @ApiOperation("迁移数据，查询所有状态，执行1")
@@ -36,6 +38,14 @@ public class FixDataController {
     @GetMapping(value = "/update_all_data")
     public ResponseEntity updateAllData() {
         issueStatusService.updateAllData();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Permission(level = ResourceLevel.SITE, roles = {InitRoleCode.SITE_ADMINISTRATOR, InitRoleCode.SITE_DEVELOPER})
+    @ApiOperation("修复累积流图脏数据")
+    @PostMapping(value = "/fix_cumulative_flow_diagram")
+    public ResponseEntity fixCumulativeFlowDiagram() {
+        reportService.fixCumulativeFlowDiagram();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
