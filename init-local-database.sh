ac02ca3ee26086909ba9e2ca9fe17194894bac84
dinghuang <dinghuang123@gmail.com>
2019-01-08 17:26:11 +0800

    [FIX] 修复版本统计图

--- init-local-database.sh
+++ init-local-database.sh
@@ -1,14 +1,12 @@
+#!/usr/bin/env bash
 mkdir -p target
-
-if [ ! -f target/hap-liquibase-tools.jar ]
+if [ ! -f target/choerodon-tool-liquibase.jar ]
 then
-    curl http://nexus.saas.hand-china.com/content/repositories/rdc/com/hand/hap/cloud/hap-liquibase-tools/1.0/hap-liquibase-tools-1.0.jar -o target/hap-liquibase-tools.jar
+    curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.9.0.RELEASE/choerodon-tool-liquibase-0.9.0.RELEASE.jar -o target/choerodon-tool-liquibase.jar
 fi
-
-# 初始化项目数据库
 java -Dspring.datasource.url="jdbc:mysql://localhost:3306/agile_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
-    -Dspring.datasource.username=root \
-    -Dspring.datasource.password=root \
-    -Ddata.init=true -Ddata.drop=false \
-    -Ddata.dir=src/main/resources \
-    -jar target/hap-liquibase-tools.jar
+ -Dspring.datasource.username=root \
+ -Dspring.datasource.password=root \
+ -Ddata.drop=false -Ddata.init=true \
+ -Ddata.dir=src/main/resources \
+ -jar target/choerodon-tool-liquibase.jar
