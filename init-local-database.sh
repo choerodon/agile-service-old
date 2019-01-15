17474bda3cb50cfc36b4093e6fbbf067be6e87b1
陈士男 <shinan.chen@hand-china.com>
2019-01-15 11:05:07 +0800

    [IMP] 调整sh

--- init-local-database.sh
+++ init-local-database.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 mkdir -p target
-if [ ! -f target/choerodon-tool-liuibase.jar ]
+if [ ! -f target/choerodon-tool-liquibase-0.9.0.RELEASE.jar ]
 then
-    curl http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.6.4.RELEASE/choerodon-tool-liquibase-0.6.4.RELEASE.jar -o target/choerodon-tool-liquibase.jar
+    wget http://nexus.choerodon.com.cn/repository/choerodon-release/io/choerodon/choerodon-tool-liquibase/0.9.0.RELEASE/choerodon-tool-liquibase-0.9.0.RELEASE.jar -P target
 fi
 java -Dspring.datasource.url="jdbc:mysql://localhost:3306/agile_service?useUnicode=true&characterEncoding=utf-8&useSSL=false" \
  -Dspring.datasource.username=root \
  -Dspring.datasource.password=root \
  -Ddata.drop=false -Ddata.init=true \
  -Ddata.dir=src/main/resources \
- -jar target/choerodon-tool-liquibase.jar
+ -jar target/choerodon-tool-liquibase-0.9.0.RELEASE.jar
