1b551054c79a450ca6d26b6964d9316fe56f8b5a
HuangFuqiang <fuqianghuang01@gmail.com>
2019-02-25 11:57:26 +0800

    [FIX]去除多余代码

--- src/main/java/io/choerodon/agile/api/dto/IssueMoveDTO.java
+++ src/main/java/io/choerodon/agile/api/dto/IssueMoveDTO.java
@@ -26,10 +26,6 @@ public class IssueMoveDTO {
 
     private Boolean rank;
 
-    private Long originColumnCount;
-
-    private Long columnCount;
-
     public Boolean getRank() {
         return rank;
     }
@@ -108,21 +104,5 @@ public class IssueMoveDTO {
 
     public void setOriginColumnId(Long originColumnId) {
         this.originColumnId = originColumnId;
-    }
-
-    public void setColumnCount(Long columnCount) {
-        this.columnCount = columnCount;
-    }
-
-    public Long getColumnCount() {
-        return columnCount;
-    }
-
-    public void setOriginColumnCount(Long originColumnCount) {
-        this.originColumnCount = originColumnCount;
-    }
-
-    public Long getOriginColumnCount() {
-        return originColumnCount;
     }
 }
