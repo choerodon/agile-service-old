94f86866389643c6cb4e9aa984664ca729bc4134
Song <jian.song01@hand-china.com>
2019-05-16 17:35:19 +0800

    [ADD]合并前端代码

--- react/src/app/agile/containers/project/IterationBoard/IterationBoardComponent/IssueType/IssueTypeWrap.js
+++ react/src/app/agile/containers/project/IterationBoard/IterationBoardComponent/IssueType/IssueTypeWrap.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Card from '../Card';
-import IssueType from './IssueType.js';
+import IssueType from './IssueType';
 
 class IssueTypeWrap extends Component {
   constructor(props) {
