295f0be35758f74dd2492949e26b03b8d807ad65
laincarl <1047972853@qq.com>
2019-06-20 11:06:13 +0800

    [IMP]无史诗下不显示无特性，没有不关联史诗的特性时，不显示无特性

--- react/src/app/agile/stores/project/StoryMap/StoryMapStore.js
+++ react/src/app/agile/stores/project/StoryMap/StoryMapStore.js
@@ -57,10 +57,10 @@ class StoryMapStore {
       const { epicWithFeature, featureWithoutEpic } = storyMapData;
       const newStoryMapData = {
         ...storyMapData, 
-        epicWithFeature: epicWithFeature.concat({
+        epicWithFeature: featureWithoutEpic.length > 0 ? epicWithFeature.concat({
           issueId: 0,
           featureCommonDOList: featureWithoutEpic,
-        }),
+        }) : epicWithFeature,
       };
       this.issueTypes = issueTypes;
       this.prioritys = prioritys;
@@ -169,13 +169,13 @@ class StoryMapStore {
         epicId,
         collapse: this.storyData[epicId] ? this.storyData[epicId].collapse : false,
         storys: [],
-        feature: {
+        feature: epicId ? { // 无史诗不显示无特性
           none: {
             storys: [],
             version: this.getInitVersions(),
             width: epicWithWidth ? epicWithWidth.width : 1,
           },
-        },
+        } : {},
       };
       const targetFeature = storyData[epicId].feature;
       epic.featureCommonDOList.forEach((feature) => {
